c4b29075eba3d77ede591f61c061e5c32103c886
KAMRUZZAMAN <kzamanbn@gmail.com>
2024-06-24 18:38:21 +0600

    Update users.blade.php

--- resources/views/livewire/users.blade.php
+++ resources/views/livewire/users.blade.php
@@ -117,7 +117,7 @@
                                                 </label>
                                             </td>
                                             <td class="px-3 py-2 text-sm">
-                                                <a href="{{ route('user.show', $user->id) }}"
+                                                <a href="#"
                                                     class="flex items-center no-underline hover:underline">
                                                     <div class="flex-shrink-0 w-10 h-10 hidden sm:table-cell">
                                                         <img class="w-full h-full rounded-full"
@@ -148,7 +148,7 @@
                                                         class="px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-l-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
                                                         Edit
                                                     </button>
-                                                    <a wire:navigate href="{{ route('user.show', $user->id) }}"
+                                                    <a wire:navigate href="#"
                                                         class="px-4 py-2 text-sm font-medium text-gray-900 bg-white border-t border-b border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
                                                         View
                                                     </a>
